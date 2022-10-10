7adb476a3e7b31d37fc10077256ba1ce4f6ab67b
DudeGFA <favouridowuagida0@gmail.com>
2022-10-10 23:35:32 +0100

    Deployed serverless app with serverless framework

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,23 @@
+import 'source-map-support/register'
+
+import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda'
+import {getAllToDo} from "../../businessLogic/ToDo";
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log("Processing Event ", event);
+    const authorization = event.headers.Authorization;
+    const auth_split = authorization.split(' ');
+    const jwtToken = auth_split[1];
+
+    const toDos = await getAllToDo(jwtToken);
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+            "items": toDos,
+        })
+    };
+};
